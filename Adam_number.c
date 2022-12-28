885331dfb3ced9370ea8d107d5aa20fc47bb2589
Suriya Varma <119718697+Suriya-varma@users.noreply.github.com>
2022-12-28 18:43:51 +0530

    #include<stdio.h> int main() {     int n,rem,rev=0,rem1,rev1=0;     scanf("%d",&n);     int a=n*n;     int t=n;     while(t!=0)     {         rem=t%10;         rev=rev*10+rem;         t=t/10;     }     int b=rev*rev;
        while(b!=0) 
        {
            rem1=b%10;
            rev1=rev1*10+rem1;
            b=b/10;
        }
         
        if(rev1==a)     
        {
            printf("True");   
        }
        else
        {
            printf("False");
        }
    }

--- Adam_number.c
+++ Adam_number.c
@@ -3,26 +3,28 @@ int main()
 {
     int n,rem,rev=0,rem1,rev1=0;
     scanf("%d",&n);
-    int a=n*n;
+    int a=n*n;   
     int t=n;
-    while(t!=0)
+    while(t!=0)   
     {
         rem=t%10;
         rev=rev*10+rem;
-        t/=10;
+        t=t/10;
     }
-    int b=rev*rev;
-    while(b!=0)
+    int b=rev*rev;  
+   
+    while(b!=0) 
     {
         rem1=b%10;
         rev1=rev1*10+rem1;
-        b/=10;
+        b=b/10;
     }
-    if(rev1==a)
+     
+    if(rev1==a)     
     {
-        printf("True");
+        printf("True");   
     }
-    else 
+    else
     {
         printf("False");
     }
